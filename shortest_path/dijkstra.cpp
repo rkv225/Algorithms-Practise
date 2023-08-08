17a40fa463613c6c6a3073575e55f4df919145d2
Rishabh Varshney <rishabh.varshney@oncehub.com>
2023-08-08 09:12:46 +0530

    dijkstra's algorithm

--- shortest_path/dijkstra.cpp
+++ shortest_path/dijkstra.cpp
@@ -1,10 +1,12 @@
 /*
-* Prim's algorithm follows strategy to traverse smallest weighted edge to unvisited nodes till all the nodes are visited.
-* This follows dijkstra's strategy of shortest path with all nodes key set to INF and then relaxing each one of them till all nodes are visited.
+* Dijkstra's algorithm solves single source shortest path problem. This follows the strategy to relax all the adjacent nodes till all the nodes are visited.
+* We keep a map to set the vertices visited and a priority queue to always take the vertex with minimum weight.
+* This only works for edges with non negative weight
 */
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<stack>
 
 #define INF 999999
 #define NIL -999999
@@ -25,10 +27,10 @@ public:
 	}
 };
 
-class MaxNode 
+class MaxNode
 {
 public:
-	int operator() (const Node &a, const Node &b)
+	int operator() (const Node& a, const Node& b)
 	{
 		return a.key > b.key;
 	}
@@ -53,34 +55,47 @@ public:
 	void AddEdge(int u, int v, int w)
 	{
 		Adj[u].push_back(v);
-		Adj[v].push_back(u);
-		W[u][v] = W[v][u] = w;
+		W[u][v] = w;
 	}
-	void Prims(int s)
+	void Dijkstra(int s)
 	{
 		V[s].key = 0;
 		PQ.push(V[s]);
 		while (!PQ.empty())
 		{
-			Node node_u = PQ.top();
-			int u = node_u.val;
+			int u = (PQ.top()).val;
 			PQ.pop();
-			if (!Visited[u])
-			{
-				Visited[u] = true; // mark as visited
-				cout << V[u].parent << "->" << u << endl;
-			}
+			Visited[u] = true;
 			for (vector<int>::iterator vit = Adj[u].begin(); vit != Adj[u].end(); vit++)
 			{
 				int v = *vit;
-				if (!Visited[v] && W[u][v] < V[v].key)
+				if (!Visited[v] && V[u].key + W[u][v] < V[v].key)
 				{
+					V[v].key = V[u].key + W[u][v];
 					V[v].parent = u;
-					V[v].key = W[u][v];
 					PQ.push(V[v]); // push to queue
 				}
 			}
 		}
+	}
+	void print_path(int dest)
+	{
+		stack<int> path;
+		int v = dest;
+		while (V[v].parent != NIL)
+		{
+			path.push(v);
+			v = V[v].parent;
+		}
+		path.push(v);
+		cout << "\nStart";
+		while (!path.empty())
+		{
+			v = path.top();
+			path.pop();
+			cout << "->" << v;
+		}
+		cout << "(destination)";
 	}
 private:
 	int n;
@@ -108,7 +123,7 @@ int main()
 	g.AddEdge(6, 7, 1);
 	g.AddEdge(6, 8, 6);
 	g.AddEdge(7, 8, 7);
-	cout << "Prim's spanning tree:" << endl;
-	g.Prims(0);
+	g.Dijkstra(0);
+	g.print_path(5);
 	return 0;
 }
