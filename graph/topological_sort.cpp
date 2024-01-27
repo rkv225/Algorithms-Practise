7653727415114b3a4d83749968ae0faa77db389b
Rishabh Varshney <rishabh.varshney@oncehub.com>
2024-01-27 15:17:16 +0530

    course schedule problem using topological sort

--- graph/topological_sort.cpp
+++ graph/topological_sort.cpp
@@ -108,3 +108,61 @@ int main()
 	g.print_topological_sorted_order();
 	return 0;
 }
+
+/*
+Problem: 
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+
+Example 2:
+Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+
+Example 3:
+Input: numCourses = 1, prerequisites = []
+Output: [0]
+
+Link: https://leetcode.com/problems/course-schedule-ii
+*/
+
+class Solution {
+public:
+    bool dfs(int v, int &o, vector<vector<int>> &adj, vector<int> &visit, vector<int> &order) {
+        visit[v] = 1;
+        for(vector<int>::iterator i = adj[v].begin(); i != adj[v].end(); i++) {
+            if(visit[*i] == 1)
+                return true;
+            if(visit[*i] == 0 && dfs(*i, o, adj, visit, order))
+                return true;
+        }
+        visit[v] = 2;
+        order[o--] = v;
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> visit(numCourses, 0);
+        vector<int> order(numCourses, -1);
+        for(vector<vector<int>>:: iterator i = prerequisites.begin(); i != prerequisites.end(); i++) {
+            vector<int> cur = *i;
+            adj[cur[1]].push_back(cur[0]);
+        }
+        bool isCycle = false;
+        int o = numCourses - 1;
+        for(int i = 0; i < numCourses; i++)
+            if(visit[i] == 0 && dfs(i, o, adj, visit, order))
+                isCycle = true;
+        if(isCycle)
+            order.clear();
+        return order;
+    }
+};
