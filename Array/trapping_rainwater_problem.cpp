35817d5fd6e94ea54925f3d923c2e0beb5653dd7
Rishabh Varshney <rishabh.varshney@oncehub.com>
2024-04-27 09:00:01 +0530

    trapping rainwater two pointer approach

--- Array/trapping_rainwater_problem.cpp
+++ Array/trapping_rainwater_problem.cpp
@@ -32,3 +32,38 @@ int main()
     vector<int> arr = { 3, 0, 0, 2, 0, 4 };
     cout << "Total water trapped: " << trappingWater(arr, arr.size()) << endl;
 }
+
+/*
+Solution: Two pointer approach
+Instead of storing the max right and max left, we know that we need minimum of them so we calculate min.
+we keep two pointers one at beginning and one at end
+we shift the pointers based on values and calculate
+
+Link: 
+https://leetcode.com/problems/trapping-rain-water
+*/
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        int l = 0;
+        int r = n - 1;
+        int max_left = h[0];
+        int max_right = h[n - 1];
+        int ans = 0;
+        while(l < r) {
+            if(max_left <= max_right) {
+                // shift left
+                l++;
+                // update max_left
+                max_left = max(max_left, h[l]);
+                ans += max_left - h[l];
+            } else {
+                r--;
+                max_right = max(max_right, h[r]);
+                ans += max_right - h[r];
+            }
+        }
+        return ans;
+    }
+};
