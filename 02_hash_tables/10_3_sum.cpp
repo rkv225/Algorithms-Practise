94a3946c02b07ddeb72f10b35ece0c746496a851
Rishabh Varshney <rishabh.varshney@oncehub.com>
2024-04-28 15:59:57 +0530

    3 sum problem

---
+++ 02_hash_tables/10_3_sum.cpp
@@ -0,0 +1,73 @@
+/*
+Problem:
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+Solution:
+In this approach, firstly, we will hash the indices of all elements in a hashMap. In case of repeated elements, the last occurence index would be stored in hashMap.
+Here also we fix a number (num[i]), by traversing the loop. But the loop traversal here for fixing numbers would leave last two indices. These last two indices would be covered by the nested loop.
+If number fixed is +ve, break there because we can't make it zero by searching after it.
+Make a nested loop to fix a number after the first fixed number. (num[j])
+To make sum 0, we would require the -ve sum of both fixed numbers. Let us say this required.
+Now, we will find the this required number in hashMap. If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet. Push it in answer vector.
+Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+Return answer vector.
+
+Time Complexity: O(n*n)
+Space Complexity: O(n)
+
+Link: https://leetcode.com/problems/3sum
+*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());     //Sorted Array
+        if(nums.size() < 3){                // Base Case 1
+            return {};
+        }
+        if(nums[0] > 0){                    // Base Case 2
+            return {};
+        }
+        unordered_map<int , int> hashMap;
+        for(int i = 0 ; i < nums.size() ; ++i){     //Hashing of Indices
+            hashMap[nums[i]] = i;
+        }
+        vector<vector<int>> answer;
+        for(int i = 0 ; i < nums.size() - 2 ; ++i){     //Traversing the array to fix the number.
+            if(nums[i] > 0){     //If number fixed is +ve, stop there because we can't make it zero by searching after it.
+                break;
+            }
+            for(int j = i + 1 ; j < nums.size() - 1 ; ++j){     //Fixing another number after first number
+                int required = -1*(nums[i] + nums[j]);    //To make sum 0, we would require the -ve sum of both fixed numbers.
+                if(hashMap.count(required) && hashMap.find(required)->second > j){ //If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+                    answer.push_back({nums[i] , nums[j] , required});
+                }
+                j = hashMap.find(nums[j])->second; //Update j to last occurence of 2nd fixed number to avoid duplicate triplets.
+            }
+            i = hashMap.find(nums[i])->second;     //Update i to last occurence of 1st fixed number to avoid duplicate triplets.
+        }
+        return answer;  //Return answer vector.
+    }
+};
